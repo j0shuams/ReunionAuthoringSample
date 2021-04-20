67d6d8adce966f3c9ab7d85871e897fe9cb8f84a
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-04-20 11:41:49 -0700

    Add project files.

---
+++ MyLibrary/CustomButton.cs
@@ -0,0 +1,62 @@
+﻿
+using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MyLibrary
+{
+    public sealed class CustomButton : Button
+    {
+        public string Text { get; private set; }
+        public bool OverrideEntered { get; set; }
+
+        public CustomButton()
+            : this("CustomButton")
+        {
+        }
+
+        public CustomButton(string text)
+        {
+            Text = text;
+            Content = text;
+            OverrideEntered = true;
+        }
+
+        protected override void OnPointerEntered(global::Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            if (!OverrideEntered)
+            {
+                base.OnPointerEntered(e);
+                return;
+            }
+
+            Text = Content?.ToString();
+            Content = "Entered";
+        }
+
+        protected override void OnPointerExited(global::Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            if (!OverrideEntered)
+            {
+                base.OnPointerExited(e);
+                return;
+            }
+
+            Content = Text;
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var size = new Size(160, 30);
+            base.MeasureOverride(size);
+            return size;
+        }
+
+        public string GetText()
+        {
+            return Text;
+        }
+    }
+}
